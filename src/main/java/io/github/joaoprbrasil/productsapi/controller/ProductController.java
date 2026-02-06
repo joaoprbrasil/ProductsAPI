71b24b29563841b9aa32a5ccb159abc3f90154e3
joaoprbrasil <joaopedrobrasil.brasil007@gmail.com>
2026-02-05 23:20:31 -0300

    add: search request

--- src/main/java/io/github/joaoprbrasil/productsapi/controller/ProductController.java
+++ src/main/java/io/github/joaoprbrasil/productsapi/controller/ProductController.java
@@ -4,7 +4,7 @@ import io.github.joaoprbrasil.productsapi.model.Product;
 import io.github.joaoprbrasil.productsapi.repository.ProductRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -35,9 +35,21 @@ public class ProductController {
         return productRepository.findById(id).orElse(null);
     }
 
+    @GetMapping
+    public List<Product> search(@RequestParam("name") String name){
+        return productRepository.findByName(name);
+    }
+
     @DeleteMapping("{id}")
     public void deleteById(@PathVariable("id") String id){
         productRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable("id") String id,
+                       @RequestBody Product product){
+        product.setId(id);
+        productRepository.save(product); // save vai atualizar o produto caso haja id na request
     }
 
 }
--- src/main/java/io/github/joaoprbrasil/productsapi/repository/ProductRepository.java
+++ src/main/java/io/github/joaoprbrasil/productsapi/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package io.github.joaoprbrasil.productsapi.repository;
 import io.github.joaoprbrasil.productsapi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
 
+    List<Product> findByName(String name);
 }
